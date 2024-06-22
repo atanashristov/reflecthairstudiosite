cb3168a846d13091be525344a806a936b416801b
Atanas (Tony) Hristov <atanas.y.hristov@gmail.com>
2024-06-22 16:08:32 -0500

    add prices

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
-  theme: {},
+  theme: {
+    extend: {
+      backgroundImage: {
+        'icon-clippers': "url('../assets/images/icons/icons8-barber-clippers-50.png')",
+      }
+    }
+  },
   plugins: [require("./plugins/openVariant")],
 };
